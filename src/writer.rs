3f4deb926ecdff9c5ce9c8b42f20dcb150ef9ae3
agustinvillegas <generalvilluguis@gmail.com>
2026-09-23 00:12:42 -0300

    loading table

--- src/writer.rs
+++ src/writer.rs
@@ -1,20 +1,18 @@
 use std::io
 use std::fs
+use create::types::*;
 
 fn save_database(table:&Table) -> Result <(), std::io::Error> { 
-	let mut output = String::new();
-	let mut cant_rows = table.rows.len();
-	let mut cant_columns = table.columns.len(); 
-	let mut file = File::create("database.db")
-	file.write_all(&cant_rows);
-	file.write_all(&cant_columns);
-	let b_c = cant_columns.to_le_bytes()?;
-	let b_r = cant_rows.to_le_bytes()?;
-	file.write_all(&b_c);
-	file.write_all(&b_r);
+	let cant_rows = table.rows.len();
+	let cant_columns = table.columns.len(); 
+	let mut file = File::create("database.db");
+	let b_c = cant_columns.to_le_bytes();
+	let b_r = cant_rows.to_le_bytes();
+	file.write_all(&b_c)?;
+	file.write_all(&b_r)?;
 		for c in &table.columns {
 			let b_n = c.name.as_bytes();
-			let n_len = n.len();
+			let n_len = c.name.len();
 			let nb_len = n_len.to_le_bytes();
 			let t =	match c.col_type {
 				Type::Int => 0,
@@ -26,7 +24,7 @@ fn save_database(table:&Table) -> Result <(), std::io::Error> {
 			file.write_all(&[t])?;
 		}
 		for r in &table.rows { 	 
-		for v in &r.Values { 
+		for v in &r.values { 
 			match v { 
 				Value::Int(n) => { 
 					let bytes = n.to_le_bytes(); 
@@ -45,5 +43,24 @@ Ok(())
 }
 
 fn load_table(file:&file) -> Result <(), std::io::Error> {
+	let mut file: i32 = file;
+	let mut buffer = [0u8,4;]
+	file.read_exact(&mut buffer)?;
+	c_c = i32::from_le_bytes(buffer);
+	file.read_exact(&mut buffer)?;
+	c_r = i32::from_le_bytes(buffer);
 	
+	for i in c_c {
+	file.read_exact(&mut buffer)?;
+	c_nlen = i32::from_le_bytes(buffer);
+	n_buffer:u32 = !vec[0u8; c_nlen] as usize;
+	file.read_exact(&mut n_buffer)?;
+	c_n = String::from_utf8(n_buffer)?;
+	file.read_exact(&mut buffer)?;
+	c_t = i32::from_le_bytes(buffer);	
+
+
+
+	}
+
 }
