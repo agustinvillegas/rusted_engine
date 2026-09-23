da091dc2a311635505cc75976cb7906b60362309
agustinvillegas <generalvilluguis@gmail.com>
2026-09-23 01:26:34 -0300

    load table function

--- src/writer.rs
+++ src/writer.rs
@@ -42,25 +42,52 @@ fn save_database(table:&Table) -> Result <(), std::io::Error> {
 Ok(())
 }
 
-fn load_table(file:&file) -> Result <(), std::io::Error> {
-	let mut file: i32 = file;
-	let mut buffer = [0u8,4;]
+fn load_table(file:&mut File) -> Result <Table, std::io::Error> {
+	let mut file = file;
+	let mut buffer = [0u8;4;]
 	file.read_exact(&mut buffer)?;
-	c_c = i32::from_le_bytes(buffer);
+	c_c = u32::from_le_bytes(buffer);
 	file.read_exact(&mut buffer)?;
-	c_r = i32::from_le_bytes(buffer);
-	
-	for i in c_c {
-	file.read_exact(&mut buffer)?;
-	c_nlen = i32::from_le_bytes(buffer);
-	n_buffer:u32 = !vec[0u8; c_nlen] as usize;
-	file.read_exact(&mut n_buffer)?;
-	c_n = String::from_utf8(n_buffer)?;
-	file.read_exact(&mut buffer)?;
-	c_t = i32::from_le_bytes(buffer);	
+	c_r = u32::from_le_bytes(buffer);
+		for i in 0..c_c {
+			file.read_exact(&mut buffer)?;
+			c_nlen = u32::from_le_bytes(buffer);
+			n_buffer:u32 = !vec[0u8; c_nlen] as usize;
+			file.read_exact(&mut n_buffer)?;
+			c_n = String::from_utf8(n_buffer)?;
+			file.read_exact(&mut buffer)?;
+			c_t = u32::from_le_bytes(buffer);	
+			let mut columns:Vec<Column> = vec::new(); 
+			let column: Column = {name: c_n, col_type: c_t};
+			columns.push(column);
+				}
+		for i in 0..c_r {
+		let mut rows:Vec<Row> = Vec::new();
+   		for i in 0..c_c {
+			match c.col_type {
+				Type::Int => {
+					let mut buffer = [0u8;4];
+					file.read_exact(&mut buffer);
+					let n:u32 = u32::from_le_bytes(buffer?);
+					rows.push(n);
+						}
+				Type::Text => {
+					file.read_exact(&mut buffer)?;
+					let t_len = u32::from_le_bytes(buffer);
+					let mut t_buffer = [0u8;&t_len]; 
+					let t: String = file.read_exact(t_buffer)?;
+					rows.push(t);		
+						}
+					}
+		
 
 
-
+				}	 
+			}
+		let table = Table {
+		 columns: columns, 
+		 rows: row,
+			};
+		Ok(table)
 	}
 
-}
